31501688596bb50b928bb721b39d13662986a0f7
Aman Prasad <amanprasad@Amans-MacBook-Pro-m2.local>
2022-10-31 11:03:43 +0530

    added elevator lld

---
+++ src/main/java/com/ratedra/elevator/ElevatorLLD.java
@@ -0,0 +1,215 @@
+package com.ratedra.elevator;
+
+import java.util.*;
+
+// assuming just one elevator in the building
+class Elevator implements Observer { // observer
+    public static final Integer TOPMOST_FLOOR = 5;
+    public static final Integer BOTTOMMOST_FLOOR = 0;
+
+    private int currentFloor;
+    private Direction currentDirection;
+    private Queue<Integer> upQueue;
+    private Queue<Integer> downQueue;
+
+    private ElevatorManager elevatorManager;
+
+    public Elevator() {
+        this.currentFloor = 0;
+        this.currentDirection = Direction.IDLE;
+        this.upQueue = new PriorityQueue<>();
+        this.downQueue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2.compareTo(o1);
+            }
+        });
+    }
+
+    @Override
+    public void update() {
+        elevatorManager.run(this);
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public void setCurrentFloor(int currentFloor) {
+        this.currentFloor = currentFloor;
+    }
+
+    public Direction getCurrentDirection() {
+        return currentDirection;
+    }
+
+    public void setCurrentDirection(Direction currentDirection) {
+        this.currentDirection = currentDirection;
+    }
+
+    public Queue<Integer> getUpQueue() {
+        return upQueue;
+    }
+
+    public void setUpQueue(Queue<Integer> upQueue) {
+        this.upQueue = upQueue;
+    }
+
+    public Queue<Integer> getDownQueue() {
+        return downQueue;
+    }
+
+    public void setDownQueue(Queue<Integer> downQueue) {
+        this.downQueue = downQueue;
+    }
+}
+
+interface Observer{
+    public void update();
+}
+
+abstract class Request{ // observable
+    private static Integer requestCounter = 0;
+
+    private List<Elevator> observers = new ArrayList<>();
+
+    public abstract Direction getDirection();
+
+    public abstract boolean addToQueue(Elevator elevator, int floor);
+
+    public void addObservers(Observer elevator){
+        observers.add((Elevator) elevator);
+    }
+
+    public void notifyObservers(){
+        this.observers.stream().forEach(o -> o.update());
+    }
+}
+
+class UpRequest extends Request{
+
+    @Override
+    public Direction getDirection() {
+        return Direction.UP;
+    }
+
+    @Override
+    public boolean addToQueue(Elevator elevator, int floor) {
+        try {
+            assert (floor >= Elevator.BOTTOMMOST_FLOOR && floor <= Elevator.TOPMOST_FLOOR);
+            Queue<Integer> upQueue = elevator.getUpQueue();
+            if (!upQueue.contains(floor)) {
+                upQueue.offer(floor);
+                notifyObservers();
+            }
+        } catch (Exception exception){
+            return false;
+        }
+        return true;
+    }
+}
+
+class DownRequest extends Request{
+
+    @Override
+    public Direction getDirection() {
+        return Direction.DOWN;
+    }
+
+    @Override
+    public boolean addToQueue(Elevator elevator, int floor) {
+        try {
+            assert (floor >= Elevator.BOTTOMMOST_FLOOR && floor <= Elevator.TOPMOST_FLOOR);
+            Queue<Integer> downQueue = elevator.getDownQueue();
+            if (!downQueue.contains(floor)) {
+                downQueue.offer(floor);
+                notifyObservers();
+            }
+        } catch (Exception e){
+            return false;
+        }
+        return true;
+    }
+}
+
+class RequestFactory{
+    private static Map<Direction, Request> requestByDirection = new HashMap<>();
+    static {
+        requestByDirection.put(Direction.UP, new UpRequest());
+        requestByDirection.put(Direction.DOWN, new DownRequest());
+    }
+
+    private static RequestFactory INSTANCE;
+
+    private RequestFactory(){}
+
+    public static RequestFactory getInstance(){
+        if(INSTANCE == null){
+            INSTANCE = new RequestFactory();
+        }
+        return INSTANCE;
+    }
+
+    public Request getRequest(Direction direction){
+        return requestByDirection.get(direction);
+    }
+}
+
+enum Direction{
+    UP,
+    DOWN,
+    IDLE;
+}
+
+class ElevatorManager{
+    public void addRequest(Elevator elevator, Direction direction, int floor){
+        RequestFactory requestFactory =  RequestFactory.getInstance();
+        Request request = requestFactory.getRequest(direction);
+
+        boolean success = request.addToQueue(elevator, floor);
+        if(success){
+            System.out.println("Request was added successfully");
+        } else{
+            System.out.println("failed to add Request");
+        }
+    }
+
+    public void run(Elevator elevator){
+        Direction currentDirection = elevator.getCurrentDirection();
+        if(currentDirection == Direction.IDLE){
+            moveUp(elevator);
+            moveDown(elevator);
+            elevator.setCurrentDirection(Direction.IDLE);
+        }
+    }
+
+    private void moveUp(Elevator elevator){
+        Queue<Integer> upQueue = elevator.getUpQueue();
+        elevator.setCurrentDirection(Direction.UP);
+        while (!upQueue.isEmpty()){
+            Integer nextFloor = upQueue.poll();
+            if(elevator.getCurrentFloor() < nextFloor) {
+                elevator.setCurrentFloor(nextFloor);
+            }
+        }
+    }
+
+    private void moveDown(Elevator elevator){
+        Queue<Integer> downQueue = elevator.getDownQueue();
+        elevator.setCurrentDirection(Direction.DOWN);
+        while (!downQueue.isEmpty()){
+            Integer nextFloor = downQueue.poll();
+            if(elevator.getCurrentFloor() > nextFloor) {
+                elevator.setCurrentFloor(nextFloor);
+            }
+        }
+    }
+
+}
+
+
+public class ElevatorLLD {
+    public static void main(String[] args) {
+
+    }
+}
