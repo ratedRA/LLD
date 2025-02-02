d7073daf88ddfd621b276fc2c4dbc44df79cbb80
Aman Prasad <amanprasad@Amans-MacBook-Pro.local>
2025-02-02 15:55:12 +0530

    remove redundant

--- src/main/java/com/ratedra/messagequeue/SimplifiedKafkaImpl.java
+++ src/main/java/com/ratedra/messagequeue/SimplifiedKafkaImpl.java
@@ -191,10 +191,6 @@ public class SimplifiedKafkaImpl {
                     });
                 }, consumerGroupExecutor);
             });
-
-            for(ConsumerGroup consumerGroup : consumerGroups){
-                consumerGroup.consumers.forEach(Consumer::consume);
-            }
         }
     }
 }
